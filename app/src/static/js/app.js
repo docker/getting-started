a2e7cddbb11ed8a228540b2f25b00d964f848dd8
MaolinPan <EnsCausaSui@outlook.com>
2021-06-10 21:32:07 +0800

    app\src\static\app.js #56 "you have no todo items yet! Add one above"

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -52,8 +52,8 @@ function TodoListCard() {
     return (
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
-            {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above!</p>
+            {items.length === 0 && ( 
+            <p className="text-center">You have no todo items yet! Add one above!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
