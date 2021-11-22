1d3cc4c0645a8f0453e821f921ced58146768010
shema landry <landry.shema@terragon.net>
2021-11-22 16:16:42 -0500

    titlechange

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above!</p>
+                <p className="text-center">Empty List!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
