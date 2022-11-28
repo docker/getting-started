87c5550d570139e79c978b51671cc3e48a65f32a
sush147 <72809526+sush147@users.noreply.github.com>
2022-11-28 17:20:58 +0530

    Update app.js

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -54,6 +54,8 @@ function TodoListCard() {
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
                 <p className="text-center">No items yet! Add one above!</p>
+                <p className="text-center">You have no todo items yet! Add one above!</p>
+ 
             )}
             {items.map(item => (
                 <ItemDisplay
