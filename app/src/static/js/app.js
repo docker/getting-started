936804a2967a2ff86fd3237ec08865eb0d47a525
Chris-Haubrich <102974098+Chris-Haubrich@users.noreply.github.com>
2023-01-22 12:09:09 -0600

    commit add button change

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Item'}
+                        {submitting ? 'Adding...' : 'Add Shit'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
